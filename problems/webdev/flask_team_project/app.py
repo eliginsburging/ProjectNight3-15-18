12ee81ebccd32572ab6c2592700798ff42334ca1
eli <eli.ginsburg.marcy@gmail.com>
2018-03-17 18:15:16 -0500

    project night code

--- problems/webdev/flask_team_project/app.py
+++ problems/webdev/flask_team_project/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
+from pprint import pprint as pp
+import pdb
+from itertools import groupby
 
 app = Flask(__name__)
 app.debug = True
@@ -9,9 +12,9 @@ app.config['SECRET_KEY'] = 'foobar'
 toolbar = DebugToolbarExtension(app)
 
 def get_names():
-    client = meetup.api.Client("3f6d3275d3b6314e73453c4aa27")
+    client = meetup.api.Client("your meetup API key here (keep quotes)")
 
-    rsvps=client.GetRsvps(event_id='235484841', urlname='_ChiPy_')
+    rsvps=client.GetRsvps(event_id='your meetup event ID here', urlname='_ChiPy_')
     member_id = ','.join([str(i['member']['member_id']) for i in rsvps.results])
     members = client.GetMembers(member_id=member_id)
 
@@ -32,8 +35,26 @@ def rsvps():
 
 @app.route('/teams', methods=['GET', 'POST'])
 def teams():
-    results = request.form.to_dict()
-    return render_template('teams.html', teams=[results])
+    all_people = request.form.to_dict()
+    all_teams = []
+    count = 0
+    team = {}
+    total_lines = 0
+    for person, num_lines in all_people.items():
+        pdb.set_trace()
+        count += 1
+        team[person] = num_lines
+        total_lines += int(num_lines)
+        if count == 4:
+            all_teams.append((team, total_lines))
+            count = 0
+            total_lines = 0
+            team = {}
+    if team:
+        all_teams.append((team, total_lines))
+    # pdb.set_trace()
+    pp(all_teams)
+    return render_template('teams.html', teams=all_teams)
 
 if __name__ == '__main__':
     app.run(debug=True)
